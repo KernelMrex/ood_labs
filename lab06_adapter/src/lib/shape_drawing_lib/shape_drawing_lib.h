66555d91588bb8462869104a352e9d788a72237a
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2023-03-13 16:00:11 +0300

    lab06_adapter добавил код, для отрисовки, который нужно было добавить

--- lab06_adapter/src/lib/shape_drawing_lib/shape_drawing_lib.h
+++ lab06_adapter/src/lib/shape_drawing_lib/shape_drawing_lib.h
@@ -27,34 +27,47 @@ class CTriangle : public ICanvasDrawable
 {
 public:
 	CTriangle(const Point& p1, const Point& p2, const Point& p3)
-	{
-		// TODO: написать код конструктора
-	}
+		: m_p1(p1)
+		, m_p2(p2)
+		, m_p3(p3)
+	{}
 
 	void Draw(graphics_lib::ICanvas& canvas) const override
 	{
-		// TODO: написать код рисования треугольника на холсте
+		canvas.MoveTo(m_p1.x, m_p1.y);
+		canvas.LineTo(m_p2.x, m_p2.y);
+		canvas.LineTo(m_p3.x, m_p3.y);
+		canvas.MoveTo(m_p2.x, m_p2.y);
+		canvas.MoveTo(m_p3.x, m_p3.y);
 	}
 
 private:
-	// TODO: дописать приватную часть
+	Point m_p1, m_p2, m_p3;
 };
 
 class CRectangle : public ICanvasDrawable
 {
 public:
 	CRectangle(const Point& leftTop, int width, int height)
-	{
-		// TODO: написать код конструктора
-	}
+		: m_leftTop(leftTop)
+		, m_width(width)
+		, m_height(height)
+	{}
 
 	void Draw(graphics_lib::ICanvas& canvas) const override
 	{
-		// TODO: написать код рисования прямоугольника на холсте
+		canvas.MoveTo(m_leftTop.x, m_leftTop.y);
+		canvas.LineTo(m_leftTop.x + m_width, m_leftTop.y);
+		canvas.LineTo(m_leftTop.x, m_leftTop.y + m_height);
+
+		canvas.MoveTo(m_leftTop.x + m_width, m_leftTop.y + m_height);
+		canvas.LineTo(m_leftTop.x + m_width, m_leftTop.y);
+		canvas.LineTo(m_leftTop.x, m_leftTop.y + m_height);
 	}
 
 private:
-	// TODO: дописать приватную часть
+	Point m_leftTop;
+	int m_width, m_height;
 };
 
 // Художник, способный рисовать ICanvasDrawable-объекты на ICanvas
@@ -62,17 +75,17 @@ class CCanvasPainter
 {
 public:
 	CCanvasPainter(graphics_lib::ICanvas& canvas)
+		: m_canvas(canvas)
 	{
-		// TODO: дописать конструктор класса
 	}
 
 	void Draw(const ICanvasDrawable& drawable)
 	{
-		// TODO: дописать код рисования ICanvasDrawable на canvas, переданном в конструктор
+		drawable.Draw(m_canvas);
 	}
 
 private:
-	// TODO: дописать приватную часть
+	graphics_lib::ICanvas& m_canvas;
 };
 
 } // namespace shape_drawing_lib
